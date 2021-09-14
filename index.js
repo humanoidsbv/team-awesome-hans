df9e9f712fdc1ba747322f4f019732a4106d64f3
Hans Charbon <hans.acc@hotmail.com>
2021-09-14 13:39:16 +0200

    UPDATE implementend comments from previous PR

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 const navButton = document.querySelector(".nav-btn");
-const navDropDown = document.getElementById("nav-dropdown")
+const navDropDown = document.getElementById("nav-dropdown");
+const header = document.getElementById("header");
 
 navButton.addEventListener("click", () => {
     navButton.classList.toggle("nav-btn-close");
     navDropDown.classList.toggle("show");
+    header.classList.toggle("header-show");
 })
