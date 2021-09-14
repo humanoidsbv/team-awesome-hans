137f2fdd25c2c84df0a884e1c57035b170714e7a
Hans Charbon <hans.acc@hotmail.com>
2021-09-14 18:42:13 +0200

    ADD desktop header. FIX breaking mobile header with opened dropdown when resizing from mobile to desktop view width.

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ navButton.addEventListener("click", () => {
     navDropDown.classList.toggle("show");
     header.classList.toggle("header-show");
 })
+
+window.addEventListener("resize", () => {
+    if(navButton.classList.contains("nav-btn-close")){
+        if(document.documentElement.clientWidth >= 1024){
+            navButton.classList.toggle("nav-btn-close");
+            navDropDown.classList.toggle("show");
+            header.classList.toggle("header-show");
+        }
+    }
+})
