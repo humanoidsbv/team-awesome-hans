96a9708812678ef396aefc80e91d3aa6f2ce1778
Hans Charbon <hans.acc@hotmail.com>
2021-09-15 11:15:40 +0200

    Fix: corrected changes requested in PR see the PR for details Reviewed-by: Ruben Werdmuller Ref: 342e664

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const navButton = document.querySelector(".nav-btn");
-const navDropDown = document.querySelector(".dropdown-content");
+const drawer = document.querySelector(".drawer-content");
 const header = document.querySelector(".header");
 
 navButton.addEventListener("click", () => {
     navButton.classList.toggle("nav-btn-close");
-    navDropDown.classList.toggle("dropdown-show");
-    header.classList.toggle("header-show");
+    drawer.classList.toggle("drawer-active");
+    header.classList.toggle("header-active");
 })
 
 window.addEventListener("resize", () => {
-    if(navDropDown.classList.contains("dropdown-show")){
+    if(drawer.classList.contains("drawer-active")){
         if(document.documentElement.clientWidth >= 1024){
             navButton.classList.toggle("nav-btn-close");
-            navDropDown.classList.toggle("dropdown-show");
-            header.classList.toggle("header-show");
+            drawer.classList.toggle("drawer-active");
+            header.classList.toggle("header-active");
         }
     }
 })
