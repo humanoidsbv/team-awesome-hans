c3e90e56ce187f67314dc013090d4dd661ad5f7f
Hans Charbon <hans.acc@hotmail.com>
2021-09-15 16:01:57 +0200

    Add review suggestions on class names based on BEM convention

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const drawer = document.querySelector(".header__drawer");
 const header = document.querySelector(".header");
 
 navButton.addEventListener("click", () => {
-    navButton.classList.toggle("header__button__close");
-    drawer.classList.toggle("header__drawer__active");
-    header.classList.toggle("header__active");
+    navButton.classList.toggle("header__button--close");
+    drawer.classList.toggle("header__drawer--active");
+    header.classList.toggle("header--active");
 })
 
 window.addEventListener("resize", () => {
-    if(drawer.classList.contains("header__drawer__active")){
+    if(drawer.classList.contains("header__drawer--active")){
         if(document.documentElement.clientWidth >= 1024){
-            navButton.classList.toggle("header__button__close");
-            drawer.classList.toggle("header__drawer__active");
-            header.classList.toggle("header__active");
+            navButton.classList.toggle("header__button--close");
+            drawer.classList.toggle("header__drawer--active");
+            header.classList.toggle("header--active");
         }
     }
 })
