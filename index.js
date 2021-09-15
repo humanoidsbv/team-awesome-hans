c40ca27a01303b7569c1bd2b76fbbc9bdd10161d
Hans Charbon <hans.acc@hotmail.com>
2021-09-15 15:11:06 +0200

    Add class names based on BEM convention

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const navButton = document.querySelector(".nav-btn");
-const drawer = document.querySelector(".drawer-content");
+const navButton = document.querySelector(".header__button");
+const drawer = document.querySelector(".header__drawer");
 const header = document.querySelector(".header");
 
 navButton.addEventListener("click", () => {
-    navButton.classList.toggle("nav-btn-close");
-    drawer.classList.toggle("drawer-active");
-    header.classList.toggle("header-active");
+    navButton.classList.toggle("header__button__close");
+    drawer.classList.toggle("header__drawer__active");
+    header.classList.toggle("header__active");
 })
 
 window.addEventListener("resize", () => {
-    if(drawer.classList.contains("drawer-active")){
+    if(drawer.classList.contains("header__drawer__active")){
         if(document.documentElement.clientWidth >= 1024){
-            navButton.classList.toggle("nav-btn-close");
-            drawer.classList.toggle("drawer-active");
-            header.classList.toggle("header-active");
+            navButton.classList.toggle("header__button__close");
+            drawer.classList.toggle("header__drawer__active");
+            header.classList.toggle("header__active");
         }
     }
 })
