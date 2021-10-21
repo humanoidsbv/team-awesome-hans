669f3f452395739ace597ef3e7a0d84a419055e3
Hans Charbon <hans.acc@hotmail.com>
2021-10-21 10:34:21 +0200

    fix small test error

--- services/__tests__/date.test.tsx
+++ services/__tests__/date.test.tsx
@@ -5,7 +5,7 @@ it("it extracts the date from an ISO string and equals to: 2 dec. 2011", () => {
   expect(date).toBe("2 dec. 2011");
 });
 
-it("it extracts the local time from an ISO string and equals to: 12:00", () => {
-  const time = getDateTime("2011-12-02T11:00:00.000Z");
-  expect(time).toBe("12:00");
+it("it extracts the local time from an ISO string and equals to: 11:00", () => {
+  const time = getDateTime("2011-12-02T11:00:00.000");
+  expect(time).toBe("11:00");
 });
