3782c9a802fd488245a42456182b88d56fc9787f
Hans Charbon <hans.acc@hotmail.com>
2021-10-21 15:09:34 +0200

    Add requested changes from previous PR

---
+++ __mocks__/mocked-time-entries.ts
@@ -0,0 +1,9 @@
+export const mockedTimeEntries = [
+  {
+    activity: "bootje varen",
+    client: "Port of Rotterdam",
+    startTime: "2021-10-08T07:01:00.000Z",
+    endTime: "2021-10-08T15:00:00.000Z",
+    id: 2,
+  },
+];
