e2dd12c08f6bda69ff847a5c4d28e58b79ffbb34
Hans Charbon <hans.acc@hotmail.com>
2021-10-21 10:45:29 +0200

    Add requested changes from previous PR

--- services/__tests__/date.test.tsx
+++ services/__tests__/date.test.tsx
@@ -1,11 +1,11 @@
 import { getDate, getDateTime } from "../date";
 
-it("it extracts the date from an ISO string and equals to: 2 dec. 2011", () => {
+test("it extracts the date from an ISO string and equals to: 2 dec. 2011", () => {
   const date = getDate("2011-12-02T11:12:00.000Z");
   expect(date).toBe("2 dec. 2011");
 });
 
-it("it extracts the local time from an ISO string and equals to: 11:00", () => {
+test("it extracts the local time from an ISO string and equals to: 11:00", () => {
   const time = getDateTime("2011-12-02T11:00:00.000");
   expect(time).toBe("11:00");
 });
